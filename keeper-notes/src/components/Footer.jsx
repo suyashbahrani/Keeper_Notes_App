c17340fa3379e00f17d76122634ebed4b1ae3ef2
Suyash Bahrani <suyash.bahrani@gmail.com>
2022-02-23 18:41:30 -0600

    Added props with tree components

--- keeper-notes/src/components/Footer.jsx
+++ keeper-notes/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-function Footer(){
-    const currentYear = new Date().getFullYear();
-    return <footer>
-        Copyright ⓒ {currentYear}
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
     </footer>
+  );
 }
 
 export default Footer;
